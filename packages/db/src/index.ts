4ee67fd80a98a4689eacded2f4e002d0766e48da
Hleb Siamionau <hleb.siamionau@creativeit.io>
2024-05-04 17:10:22 +0200

    add time entry entities

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,13 +3,14 @@ import { createClient } from "@libsql/client";
 
 import { dbEnv } from "./env";
 import * as auth from "./schema/auth";
+import * as timeEntry from "./schema/timeEntry";
 
 export * from "drizzle-orm/sql";
 
-export const schema = { ...auth, };
+export const schema = { ...auth, timeEntry };
 
 const turso = createClient({
-  url: dbEnv.TURSO_DATABASE_URL!,
+  url: dbEnv.TURSO_DATABASE_URL,
   authToken: dbEnv.TURSO_AUTH_TOKEN,
 });
 
